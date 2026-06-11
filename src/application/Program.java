98373c4e8d5dc21464964d04a7b2d1d2e062bfb8
Flavio Yukio <flavio_ohphata@hotmail.com>
2026-06-11 13:40:04 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,5 +33,11 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
+		
+		System.out.println("=== Test 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine" );
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 }
